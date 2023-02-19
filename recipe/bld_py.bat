a73babf8306dfc752e403da476158a74c6d844f2
Lori A. Burns <lori.burns@gmail.com>
2023-02-19 14:09:54 -0500

    driver vs pkg py

--- recipe/bld_py.bat
+++ recipe/bld_py.bat
@@ -1,9 +1,11 @@
 
-cmake ${CMAKE_ARGS} -G"Ninja" ^
+cmake ${CMAKE_ARGS} ^
+      -G"Ninja" ^
       -S"%SRC_DIR%\pylibxc" ^
       -Bbuild_py ^
       -DCMAKE_BUILD_TYPE=Release ^
-      -DCMAKE_INSTALL_PREFIX="%PREFIX%"
+      -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
+      -DPYLIBXC_INSTALL_PYMODDIR="\Lib\site-packages"
 if errorlevel 1 exit 1
 
 cmake --build build_py ^
--- recipe/build_py.sh
+++ recipe/build_py.sh
@@ -3,8 +3,9 @@ ${BUILD_PREFIX}/bin/cmake ${CMAKE_ARGS} \
     -G"Ninja" \
     -S${SRC_DIR}/pylibxc \
     -Bbuild_py \
+    -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DCMAKE_BUILD_TYPE=Release
+    -DPYLIBXC_INSTALL_PYMODDIR="/lib/python${PY_VER}/site-packages"
 
 ${BUILD_PREFIX}/bin/cmake --build build_py --target install
 
