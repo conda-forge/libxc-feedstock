83a1df207f7f7c2c0f35a363c799a17b9faa63a6
William Dawson <william.dawson@riken.jp>
2023-05-31 11:27:41 +0900

    Should fix arm by disabling check when cross compiling

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,5 +8,9 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 autoreconf --install  # autotools based install officially recommended for now it seems...
 CFLAGS="-O2 -g $CFLAGS" ./configure --prefix="$PREFIX" --enable-shared
 make -j${CPU_COUNT}
-make check || ( set -x; cat testsuite/test-suite.log; exit 1 )
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  make check || ( set -x; cat testsuite/test-suite.log; exit 1 )
+fi
+
 make install
