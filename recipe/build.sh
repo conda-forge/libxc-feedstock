6c06dac3faa9a87ffecfe1d7f37bb815d0f4c59e
William Dawson <william.dawson@riken.jp>
2023-05-31 10:50:45 +0900

    Enable arm64 build of version 4
    https://conda-forge.org/blog/posts/2020-10-29-macos-arm64/

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 set -e
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 autoreconf --install  # autotools based install officially recommended for now it seems...
 CFLAGS="-O2 -g $CFLAGS" ./configure --prefix="$PREFIX" --enable-shared
 make -j${CPU_COUNT}
